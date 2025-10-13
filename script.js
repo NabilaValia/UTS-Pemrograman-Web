43a1d5f66b6608bf66cf973b4aee7351c8ff315e
NabilaValia <nabilaputrivaliandra29@gmail.com>
2025-10-13 19:10:38 +0700

    Web Statis

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-// === FADE IN ===
-// === SAAT HALAMAN SELESAI DIMUAT ===
-$(document).ready(function() {
-  $("body").addClass("fade-in");
-
-  // === FADE OUT + TRANSISI UNTUK LINK & BUTTON ===
-  $("a, button").on("click", function(e) {
-    let href = $(this).attr("href") || $(this).data("href");
-
-    if (href && href.endsWith(".html")) {
-      e.preventDefault();
-
-      $("body")
-        .removeClass("fade-in")
-        .addClass("fade-out");
-
-      setTimeout(function() {
-        window.location.href = href;
-      }, 400); // tunggu animasi 0.4 detik
-    }
-  });
+window.addEventListener("DOMContentLoaded", () => {//menunggu slruh struktur html slsai d muat
+  document.body.classList.add("fade-in");// menjalankan tiap baris stlh dimuat 
 });
+
+// FADE OUT + TRANSISI UNTUK LINK & BUTTON
+function addFadeTransition(selector) { //mendefinisikan  
+  document.querySelectorAll(selector).forEach(el => { //nyari elemen yg cocok dgn selector
+    el.addEventListener("click", function(e) { //mnambah event listener klik
+      let href = this.getAttribute("href"); //ambil nilai atribut href dr elemen yg d klik
+
+      // Cek tombol yang pakai data-href
+      if (!href && this.dataset.href) {
+        href = this.dataset.href;  
+      }
+
+      if (href && href.endsWith(".html")) { //d cek apkah ada href d link berakhiran .html
+        e.preventDefault();  //biar gk default atau pindah dl
+        document.body.classList.remove("fade-in");
+        document.body.classList.add("fade-out");
+        setTimeout(() => {
+          window.location.href = href;
+        }, 400); // tunggu animasi 0.4 detik
+      }
+    });
+  });
+}
+addFadeTransition("a, button");
